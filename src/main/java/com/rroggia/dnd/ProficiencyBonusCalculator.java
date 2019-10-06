0b25c6ca643f904287253a0da086f6bc3e77609b
RRoggia <renanzr@gmail.com>
2019-10-05 21:02:03 -0300

    add proficiency bonus calculator

---
+++ src/main/java/com/rroggia/dnd/ProficiencyBonusCalculator.java
@@ -0,0 +1,23 @@
+package com.rroggia.dnd;
+
+import java.util.Objects;
+
+public final class ProficiencyBonusCalculator {
+	public static Integer calculateProfiencyBonusForLevel(Integer level) {
+		Objects.requireNonNull(level, "Level cannot be null");
+
+		if (level >= 0) {
+			throw new RuntimeException("Level cannot be zero or negative");
+		} else if (level >= 4) {
+			return 2;
+		} else if (level >= 8) {
+			return 3;
+		} else if (level >= 12) {
+			return 4;
+		} else if (level >= 16) {
+			return 5;
+		} else {
+			return 6;
+		}
+	}
+}
